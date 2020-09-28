3d4af8cf0fe98e7383f0a531d1a92d06fd786b6c
royer00 <royerkevin@outlook.com>
2020-09-28 15:47:44 -0500

    Add project files.

---
+++ TenDigitFibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenDigitFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value = 0;
+            int preValue = 0;
+            int superPreValue = 1;
+            List<int> values = new List<int>();
+            //terminating loop when length of value hits 10 digits
+            while (value.ToString().Count() < 10)
+            {
+                Console.WriteLine(value);
+                value = preValue + superPreValue;
+                superPreValue = preValue;
+                preValue = value;
+                //add value to list to keep track of indexes
+                values.Add(value);
+            }
+
+            //the index after the last index will have 10 digits
+            Console.WriteLine(values.Count + 1);
+
+
+
+
+        }//end main
+    }
+}
